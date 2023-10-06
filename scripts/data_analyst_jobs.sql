29f2cd6f6bcad84a53c4d580e283aa1a94cfe0cf
seanmalley <sean.malley1@protonmail.com>
2023-10-05 20:06:44 -0400

    Bonus finished, 9 fixed

--- scripts/data_analyst_jobs.sql
+++ scripts/data_analyst_jobs.sql
@@ -74,10 +74,10 @@ WHERE location = 'CA';
 
 -- Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations.
 
-SELECT company, AVG(star_rating) AS average_star_rating
+SELECT company, AVG(star_rating) AS average_star_rating, SUM(review_count)
 FROM data_analyst_jobs
+WHERE review_count > 5000
 GROUP BY company
-HAVING SUM(review_count) > 5000
 ORDER BY company ASC;
 
 --Q 9B How many companies are there with more that 5000 reviews across all locations? (40)
